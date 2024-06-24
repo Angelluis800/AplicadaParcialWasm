04c51a5d767b7c4036197ef686eac922f48ce080
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-06-24 11:30:35 -0400

    Inyectando Servicios en el program

--- ArticuloApi.Api/Program.cs
+++ ArticuloApi.Api/Program.cs
@@ -1,4 +1,5 @@
 using ArticuloApi.Api.DAL;
+using ArticuloApi.Api.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddSwaggerGen();
 //Inyectando ConnectionStrings
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
 builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlite(ConStr));
+//Inyectando Servicios
+builder.Services.AddScoped<ArticuloService>();
 
 var app = builder.Build();
 
