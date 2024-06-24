15fb213a04f4baf32d4dc39f457f32ccab6c7c77
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-06-24 12:17:08 -0400

    Agregando Servicio de Articulo

---
+++ AplicadaParcialWasm.Client/Services/ArticuloService.cs
@@ -0,0 +1,57 @@
+﻿using Shared.Models;
+using Shared.Services;
+using System.Net.Http.Json;
+
+namespace AplicadaParcialWasm.Client.Services;
+
+public class ArticuloService(HttpClient httpClient) : IClienteService<Articulos>
+{
+    public async Task<List<Articulos>> GetAllAsync()
+    {
+        var resultado = await httpClient.GetAsync("api/Articulos");
+        if (resultado.IsSuccessStatusCode)
+            return (await resultado.Content.ReadFromJsonAsync<List<Articulos>>())!;
+
+        return null!;
+    }
+
+    public async Task<Articulos> GetByIdAsync(int id)
+    {
+        var resultado = (await httpClient.GetAsync($"api/Articulos/{id}"))!;
+
+        if (resultado.IsSuccessStatusCode)
+            return (await resultado.Content.ReadFromJsonAsync<Articulos>())!;
+
+        return null!;
+    }
+
+    //
+    public async Task<bool> ExisteDescripcionAsync(int id, string descripcion)
+    {
+        var response = await httpClient.GetAsync($"api/articulos/existe-descripcion?id={id}&descripcion={descripcion}");
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadFromJsonAsync<bool>();
+    }
+
+    public async Task<Articulos> CreateAsync(Articulos articulo)
+    {
+        var resultado = await httpClient.PostAsJsonAsync("api/Articulos", articulo);
+
+        if (resultado.IsSuccessStatusCode)
+            return (await resultado.Content.ReadFromJsonAsync<Articulos>())!;
+
+        return null!;
+    }
+
+    public async Task<bool> UpdateAsync(int id, Articulos articulo)
+    {
+        var resultado = await httpClient.PutAsJsonAsync($"api/Articulos/{id}", articulo);
+        return resultado.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var resultado = await httpClient.DeleteAsync($"api/Articulos/{id}");
+        return resultado.IsSuccessStatusCode;
+    }
+}
