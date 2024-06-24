9d548b5d40cb835460867edbaa7c54d8ad6b8ac6
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-06-24 12:17:19 -0400

    Inyectando Servicios

--- AplicadaParcialWasm.Client/Program.cs
+++ AplicadaParcialWasm.Client/Program.cs
@@ -1,11 +1,16 @@
+using AplicadaParcialWasm.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Shared.Models;
+using Shared.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 //Inyectando HttpClient
 builder.Services.AddScoped(sp => new HttpClient 
 { 
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
+
+builder.Services.AddScoped<IClienteService<Articulos>, ArticuloService>();
 
 await builder.Build().RunAsync();
