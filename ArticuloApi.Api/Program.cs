1ef54a4c9749424ff6e971c683a89411b7b68520
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-06-24 11:23:40 -0400

    Inyectando Connection Strings en el Program

--- ArticuloApi.Api/Program.cs
+++ ArticuloApi.Api/Program.cs
@@ -1,3 +1,6 @@
+using ArticuloApi.Api.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Inyectando ConnectionStrings
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlite(ConStr));
 
 var app = builder.Build();
 
