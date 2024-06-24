5955ccd119f92f3e859d8dedb7120325811e3965
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-06-24 10:51:00 -0400

    Agregando Interfaz genérica de servicios en el proyecto Shared

---
+++ Shared/IClienteServicio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared;
+
+public interface IClienteServicio<T>
+{
+    public Task<List<T>> GetTodoAsync();
+    public Task<T> GetPorIdAsync(int id);
+    public Task<T> CrearAsync(T articulo);
+    public Task<bool> ModificarAsync(int id, T articulo);
+    public Task<bool> EliminarAsync(int id);
+    public Task<bool> ExisteDescripcionAsync(int id, string descripcion);
+}
