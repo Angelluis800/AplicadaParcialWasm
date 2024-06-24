b50a81a377135b02a88a8b8ae10498f151c9a185
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-06-24 11:16:49 -0400

    Creando Contexto y tabla de Articulos

---
+++ ArticuloApi.Api/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+using System.Collections.Generic;
+
+namespace ArticuloApi.Api.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<Articulos> Articulos { get; set; }
+}
